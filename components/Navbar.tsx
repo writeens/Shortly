a13bccf8d1c0ea442d083a8b8fabfea067581616
Adewusi Victor <glove.adewusi@gmail.com>
2021-03-26 13:12:32 +0100

    Setup Navbar Component And Responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,56 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import LogoSVG from './LogoSVG';
 
 const Navbar = () => {
-  console.log('object');
+  const [showMenu, setShowMenu] = useState(false);
+  const handleToggleNavbar = () => {
+    setShowMenu((prev) => !prev);
+  };
+
+  //   const handleResize = () => {
+
+  //   }
+
+  //   useEffect(() => {
+  //       window.addEventListener('resize', handleResize)
+  //   }, [])
+
   return (
-    <nav className="w-full min-w-full flex flex-row items-center justify-between border-red-500 py-3 border font-poppins font-bold px-4 md:px-12 lg:px-28">
-      <div className="flex items-center">
+    <nav className="w-full min-w-full flex flex-row items-center justify-between py-3 font-poppins font-bold px-4 md:px-12 lg:px-28 relative">
+      {/** TOGGLE */}
+      <button type="button" onClick={handleToggleNavbar} className="w-8 h-8 absolute right-3 sm:hidden focus:ring-sDarkViolet">
+        <i className="fa fa-bars fa-lg text-sGreyViolet" />
+      </button>
+      <div className="flex items-center w-full">
         <Link href="/">
-          <a className=" text-3xl text-sVeryDarkBlue mr-12">Shortly</a>
+          <div className="mr-6 sm:mr-12">
+            <LogoSVG />
+          </div>
         </Link>
-        <div className=" text-sGreyViolet">
-          <Link href="/">
-            <a className="mr-4 text-sm hover:text-sVeryDarkBlue">Features</a>
-          </Link>
-          <Link href="/">
-            <a className="mr-4 text-sm hover:text-sVeryDarkBlue">Pricing</a>
-          </Link>
-          <Link href="/">
-            <a className="mr-4 text-sm hover:text-sVeryDarkBlue">Resources</a>
-          </Link>
+        <div className={`flex-1 absolute sm:relative flex-col sm:flex-row sm:items-center sm:justify-between ${showMenu ? '' : 'hidden'} sm:flex top-16 sm:top-0 right-0 left-0 mx-6 sm:mx-0 rounded-lg sm:rounded-none bg-sDarkViolet sm:bg-transparent pb-3 sm:pb-0 px-6 sm:px-0`}>
+          <div className=" text-white sm:text-sGreyViolet flex-col flex sm:flex-row">
+            <Link href="/">
+              <a className="sm:mr-4 text-sm make-white hover:text-sVeryDarkBlue my-2 sm:my-0 text-center">Features</a>
+            </Link>
+            <Link href="/">
+              <a className="sm:mr-4 text-sm make-white hover:text-sVeryDarkBlue my-2 sm:my-0 text-center">Pricing</a>
+            </Link>
+            <Link href="/">
+              <a className="sm:mr-4 text-sm make-white hover:text-sVeryDarkBlue my-2 sm:my-0 text-center">Resources</a>
+            </Link>
+          </div>
+          <div className="text-sm flex-col flex sm:flex-row">
+            <Link href="/">
+              <a className="px-6 py-2 sm:mr-3 make-white sm:text-sGreyViolet hover:text-sVeryDarkBlue my-2 sm:my-0 text-center">Login</a>
+            </Link>
+            <Link href="/">
+              <a className="px-6 py-2 bg-sCyan text-white rounded-2xl cursor-pointer hover:bg-opacity-70 hover:text-opacity-80 text-center">Sign Up</a>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="text-sm">
-        <Link href="/">
-          <a className="px-6 py-2 mr-3 text-sGreyViolet hover:text-sVeryDarkBlue">Login</a>
-        </Link>
-        <Link href="/">
-          <a className="px-6 py-2 bg-sCyan text-white rounded-2xl cursor-pointer hover:opacity-70">Sign Up</a>
-        </Link>
+
       </div>
     </nav>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet" />
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous" />
         </Head>
         <body>
           <Main />
